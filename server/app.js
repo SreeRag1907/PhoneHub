0aab3e919388a86d0ff051d117d68161eb9b8fcc
C.SREERAG <c.sreerag17@gmail.com>
2024-06-16 17:52:34 +0530

    changing app.js

--- server/app.js
+++ server/app.js
@@ -1,55 +1,8 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 
-const stripe = require("stripe")("sk_test_51PRcK7IuK6CXozMpf6h9Jp0pK8lNgSlQV8cSXXDcUWJVTX8SVZNA5k4AuMtCVXjaVi8cJ1MEXriJCxdiq3VKMihY00IzOG0HFy");
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.use(express.json());
-app.use(cors());
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// Checkout API
-app.post("/api/create-checkout-session", async (req, res) => {
-    try {
-        const { products } = req.body;
-
-        const lineItems = products.map((product) => {
-            // Remove dollar sign and commas, then convert to number
-            const unitAmount = parseFloat(product.price.replace(/[$,]/g, "")) * 100;
-            if (isNaN(unitAmount)) {
-                throw new Error(`Invalid price for product ${product.model}`);
-            }
-
-            return {
-                price_data: {
-                    currency: "usd", // Assuming the currency is USD
-                    product_data: {
-                        name: product.model,
-                    },
-                    unit_amount: unitAmount,
-                },
-                quantity: product.quantity,
-            };
-        });
-
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            line_items: lineItems,
-            mode: "payment",
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cancel",
-        });
-
-        res.json({ id: session.id });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-app.get("/", (req, res) => {
-    res.send("Hello, Express is running!");
-});
-
-app.listen(7000, () => {
-    console.log("Server started on port 7000");
-});
+module.exports = app;
